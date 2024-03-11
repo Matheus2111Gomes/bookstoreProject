be14f74f67b5efad0f4c50d3b184a101f3c14f4e
MatheusGomes <matheus2111gomes@gmail.com>
2024-03-11 18:24:29 -0300

    starting serverless-book config

--- serverless-books/serverless.ts
+++ serverless-books/serverless.ts
@@ -1,38 +1,89 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
+import { hello } from './src/functions';
 
 const serverlessConfiguration: AWS = {
   service: 'serverless-books',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  frameworkVersion: '3.38.0',
+  useDotenv: true,
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+      packager: 'yarn',
+    },
+    stage: '${opt:stage, self:provider.stage}',
+    stages: ['staging', 'production'],
+    prune: {
+      automatic: true,
+      number: 3,
+    },
+    alerts: {
+      dashboards: true,
+      definitions: {
+        '5XXErrors': {
+          name: '5XXErrors',
+          namespace: 'AWS/ApiGateway',
+          metric: '5XXError',
+          omitDefaultDimension: true,
+          dimensions: [
+            {
+              Name: 'ApiName',
+              Value: '${self:service}-${self:custom.stage}',
+            },
+            {
+              Name: 'Stage',
+              Value: '${self:custom.stage}',
+            },
+          ],
+          threshold: 5,
+          statistic: 'Sum',
+          period: 60,
+          evaluationPeriods: 1,
+          datapointsToAlarm: 1,
+          comparisonOperator: 'GreaterThanOrEqualToThreshold',
+        },
+      },
+      alarms: ['functionThrottles', 'functionErrors', '5XXErrors'],
+    },
+  },
+  plugins: [
+    'serverless-webpack',
+    //'serverless-offline',
+    'serverless-stage-manager',
+    'serverless-prune-plugin',
+    'serverless-plugin-aws-alerts',
+  ],
   provider: {
     name: 'aws',
-    runtime: 'nodejs14.x',
+    runtime: 'nodejs20.x',
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: ['codedeploy:*'],
+        Resource: '*',
+      },
+    ],
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
+      metrics: false, // activate to see CacheHits and Misses
+    },
+    logs: {
+      // activate to see API Gateway logs
+      restApi: {
+        accessLogging: false,
+        executionLogging: false,
+        level: 'INFO',
+        fullExecutionData: false,
+      },
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    lambdaHashingVersion: '20201221',
   },
-  // import the function via paths
   functions: { hello },
-  package: { individually: true },
-  custom: {
-    esbuild: {
-      bundle: true,
-      minify: false,
-      sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
-      concurrency: 10,
-    },
-  },
 };
 
 module.exports = serverlessConfiguration;
